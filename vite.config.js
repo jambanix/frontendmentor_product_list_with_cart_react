e19b6e9ba24518f29f697b52e86925a2b9314560
jambanix <jambanix@pm.me>
2024-10-21 17:41:48 +0100

    add base to vite config

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,5 @@ import svgr from "vite-plugin-svgr"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
+  base: "/frontendmentor_product_list_with_cart_react/"
 })
