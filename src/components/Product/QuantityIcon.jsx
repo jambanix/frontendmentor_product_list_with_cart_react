5cf8cf1393d8d1364029886b0821045aab71306b
jambanix <jambanix@pm.me>
2024-10-21 18:40:49 +0100

    add . to image path on all icons/images

--- src/components/Product/QuantityIcon.jsx
+++ src/components/Product/QuantityIcon.jsx
@@ -7,7 +7,7 @@ export const QuantityIcon = ({onClick, increment=true}) => {
         <div
         className="border-white rounded-full p-1 h-6 w-6 border-2 flex items-center justify-center hover:bg-white"
         onClick={onClick}>
-            <img src={`/assets/images/icon-${increment ? "increment" : "decrement"}-quantity.svg`}
+            <img src={`./assets/images/icon-${increment ? "increment" : "decrement"}-quantity.svg`}
             alt={`${increment ? "increment" : "decrement"} icon`}
             className="h-3 w-4 hover:fill-red" />
         </div>
