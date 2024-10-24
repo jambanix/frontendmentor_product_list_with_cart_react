08f9db6e2ab50ccac903a07dcf9c9cd8189890db
jambanix <jambanix@pm.me>
2024-10-24 22:50:13 +0100

    remove redundant component, update README

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# Frontend Mentor - Intro component with sign up form solution
+# Frontend Mentor - Product List with Cart
 
-This is a solution to the [Intro component with sign up form challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/intro-component-with-signup-form-5cf91bd49edda32581d28fd1). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+This is a solution to the [Product List with Cart](https://www.frontendmentor.io/challenges/product-list-with-cart-5MmqLVAp_d). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
 
 ## Table of contents
 
--- src/components/Modal/ConfirmationModal.jsx
+++ src/components/Modal/ConfirmationModal.jsx
@@ -1,43 +1,57 @@
 import { useProductsContext } from "../../context/ProductsProvider";
 import { useModalContext } from "../../context/ModalProvider";
-import { ModalHeading } from "./ModalHeading";
 import { ModalBody } from "./ModalBody";
 
 export const ConfirmationModal = () => {
+  const { cartItems, cartTotal, reset } = useProductsContext();
 
-    const {cartItems, cartTotal, reset} = useProductsContext();
+  const modalContext = useModalContext();
+  const hide = () => modalContext.toggleModal(false);
 
-    const modalContext = useModalContext();
-    const hide = () => modalContext.toggleModal(false);
+  const handleStartNew = () => {
+    hide();
+    reset();
+  };
 
-    const handleStartNew = () => {
-        hide();
-        reset();
-    }
-
-    return (
-        <section
-            className={`
+  return (
+    <section
+      className={`
                 fixed inset-0 flex justify-center 
                 items-center transition-colors z-20 p-4
-                ${modalContext.showModal ? "visible bg-black/80" : "invisible"}`}
-
-            onClick={hide}>
-            <div
-                className={`
+                ${
+                  modalContext.showModal ? "visible bg-black/80" : "invisible"
+                }`}
+      onClick={hide}
+    >
+      <div
+        className={`
                     rounded-lg bg-white text-black
                     flex flex-col gap-6 transition-all
                     p-8 w-full md:w-3/5 lg:w-1/2 h-fit md:h-fit max-w-[800px]
-                    ${modalContext.showModal ? "scale-100 opacity-100" : "scale-125 opacity-0"}`}
-                    
-                onClick={(e) => e.stopPropagation()}>
-                <ModalHeading/>
-                <ModalBody items={cartItems()} total={cartTotal()}/>
-                <button
-                className="rounded-full bg-red text-white h-12 hover:bg-red-dark" onClick={handleStartNew}>
-                    Start New Order
-                </button>
-            </div>
-        </section>
-    )
-}
+                    ${
+                      modalContext.showModal
+                        ? "scale-100 opacity-100"
+                        : "scale-125 opacity-0"
+                    }`}
+        onClick={(e) => e.stopPropagation()}
+      >
+        <img
+          src="./assets/images/icon-order-confirmed.svg"
+          alt="confirm order icon"
+          className="h-12 w-12"
+        />
+        <div>
+          <h2 className="text-3xl font-bold">Order confirmed</h2>
+          <p className="text-rose-400">We hope you enjoy your food!</p>
+        </div>
+        <ModalBody items={cartItems()} total={cartTotal()} />
+        <button
+          className="rounded-full bg-red text-white h-12 hover:bg-red-dark"
+          onClick={handleStartNew}
+        >
+          Start New Order
+        </button>
+      </div>
+    </section>
+  );
+};
