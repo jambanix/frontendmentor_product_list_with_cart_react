980e50cefef296745895bcc5adc64200ee7d0851
jambanix <jambanix@pm.me>
2024-10-25 12:33:23 +0100

    fix formatting on component

--- src/components/Cart/ConfirmButton.jsx
+++ src/components/Cart/ConfirmButton.jsx
@@ -1,24 +1,24 @@
 import { useProductsContext } from "../../context/ProductsProvider";
-import { useRef } from "react"
+import { useRef } from "react";
 import { ConfirmationModal } from "../Modal/ConfirmationModal";
 
-export const ConfirmButton = ({children}) => {
+export const ConfirmButton = ({ children }) => {
+  const { cartItems } = useProductsContext();
+  const cartEmpty = cartItems.length === 0;
 
-    const {cartItems} = useProductsContext();
-    const cartEmpty = cartItems.length === 0;
+  const modal = useRef();
 
-    const modal = useRef();
+  const handleClick = () => !cartEmpty && modal.current.show();
 
-    const handleClick = () => !cartEmpty && modal.current.show();
-    
-    return (
-        <>
-					<ConfirmationModal ref={modal}/>
-					<button
-							className="w-full bg-red text-white p-4 rounded-lg hover:bg-red-dark"
-							onClick={handleClick}>
-									{children}
-					</button>
-        </>
-    )
-}
+  return (
+    <>
+      <ConfirmationModal ref={modal} />
+      <button
+        className="w-full bg-red text-white p-4 rounded-lg hover:bg-red-dark"
+        onClick={handleClick}
+      >
+        {children}
+      </button>
+    </>
+  );
+};
